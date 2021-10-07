d846246c17b7c685ac2acf88d1a6cd9f4a3462a3
Yuval Barak <yuvalyehudab@mail.tau.ac.il>
2021-10-07 13:57:20 +0300

    test passing POST request

--- tests/app_test.py
+++ tests/app_test.py
@@ -10,3 +10,9 @@ def test_index():
 
     rv = web.get('/hello', follow_redirects=True)
     assert rv.status_code == 200
+    assert b"Fill Out This Form" in rv.data
+
+    data = {'name': 'Zed', 'greet': 'Hola'}
+    rv = web.post('/hello', follow_redirects=True, data=data)
+    assert b'Zed' in rv.data
+    assert b'Hola' in rv.data
