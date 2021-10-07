69282e4d8b1c820f45f6ec368ab9ea1aec08cabc
Yuval Barak <yuvalyehudab@mail.tau.ac.il>
2021-10-07 13:39:35 +0300

    make tests run - flask docs!

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,8 +1,12 @@
 import pytest
-from gothonweb import app
+from gothonweb.app import app
 
 app.config['TESTING'] = True
 web = app.test_client()
 
 def test_index():
-    pass
+    rv = web.get('/', follow_redirects=True)
+    assert rv.status_code == 404
+
+    rv = web.get('/hello', follow_redirect=True)
+    assert rv.status_code == 200
